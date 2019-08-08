745a03367f753cdb069bd6f560c0f9c6e1c35a30
Carlos <carlosaclargacha@gmail.com>
2019-08-08 08:25:51 -0500

    Layout para la aplicacion

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
-<h1>Hello world</h1>
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="text-center">Amino-ARN-ADN Traductor</h1>
+        </div>
 
-<p>This is going to be the first deploy from this PC</p>
+        <div class="container">
+            <form>
+                <div class="form-group">
+                    <label for="aminoTextInput">Aminoacidos</label>
+                    <textarea class="form-control" id="aminoTextInput" rows="3" placeholder="Escribir secuencia de Aminoacidos aqui"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="aminoTextInput">ARN</label>
+                    <textarea class="form-control" id="arnTextInput" rows="3" placeholder="Traduccion ARN aqui" readonly></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="aminoTextInput">ADN</label>
+                    <textarea class="form-control" id="adnTextInput" rows="3" placeholder="Traduccion ADN aqui" readonly></textarea>
+                </div>
+                <button type="button" class="btn btn-primary btn-lg btn-block">Traducir</button>
+            </form>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
